c77b9270736139df20e52f65c33c571c5c9d474c
Luz Davico <mldavico@gmail.com>
2022-05-25 21:23:35 -0300

    Agregada navbar simple con Bootstrap

---
+++ src/components/NavBar.jsx
@@ -0,0 +1,36 @@
+import { Nav, NavDropdown, Container, Navbar } from 'react-bootstrap';
+
+
+function NavBar() {
+    return (
+      <Navbar bg="danger" variant="dark" expand="lg">
+        <Container>
+          <Navbar.Brand href="#">
+          <img
+          alt=""
+          src="logo.svg"
+          width="30"
+          height="30"
+          className="d-inline-block align-top"
+        />{' '}
+        Not Montagne</Navbar.Brand>
+          <Navbar.Toggle aria-controls="basic-navbar-nav" />
+          <Navbar.Collapse id="basic-navbar-nav">
+            <Nav className="me-auto">
+            <NavDropdown title="Productos" id="basic-nav-dropdown">
+                <NavDropdown.Item href="#">Camping</NavDropdown.Item>
+                <NavDropdown.Item href="#">Pesca</NavDropdown.Item>
+                <NavDropdown.Divider />
+                <NavDropdown.Item href="#">Ropa Hombre</NavDropdown.Item>
+                <NavDropdown.Item href="#">Ropa Mujer</NavDropdown.Item>
+              </NavDropdown>
+              <Nav.Link href="#">La empresa</Nav.Link>
+              <Nav.Link href="#">Contacto</Nav.Link>
+            </Nav>
+          </Navbar.Collapse>
+        </Container>
+      </Navbar>
+    );
+  }
+
+export default NavBar;
